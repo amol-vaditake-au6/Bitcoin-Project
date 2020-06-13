182797f9cb97e470bbcac8c9540e3580df38e75d
Amol Vaditake <amolvaditake123@gmail.com>
2020-06-13 11:59:05 +0530

    Project pushed

---
+++ Project/project.py
@@ -0,0 +1,86 @@
+import requests
+import time
+import argparse
+from datetime import datetime
+
+iftt_link = "https://maker.ifttt.com/trigger"
+fetch_price_link = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+key = 'd7GMZJWMxA4dLaglzWHi6ohkROrxL6KN5mBwlEMXYV2'
+# Applet for send data to twitter
+twitter_applet = '{}/twitter_notifier/with/key/{}'.format(iftt_link, key)
+# Applet for send data to telegram
+telegram_applet = '{}/telegram_notifier/with/key/{}'.format(iftt_link, key)
+
+
+# Function for fetch the price form url (GET Request)
+def price():
+    response = (requests.get(fetch_price_link)).json()
+    value = response['bpi']['USD']['rate']
+    value = float(value.replace(",", ""))
+    date = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    return [date, value]
+
+
+# Function for Sending fetched Price to Twitter and Telegram(POST Request)
+def send_data(sleepTime, lowerLimit, upperLimit):
+    arr = []
+    while True:
+        price_update = price()
+        current_price = price_update[1]
+        # send prices when price falling below lower limit
+        if current_price < lowerLimit:
+            data = {"value1": "Emergrncy Update ==> price falling down",
+                    "value2": str(price_update)[1:-1]
+                    }
+            requests.post(twitter_applet, json=data)
+            requests.post(telegram_applet, json=data)
+        # send prices when price going up above upper limit
+        if current_price > upperLimit:
+            data = {"value1": "Emergrncy Update ==> price going Up",
+                    "value2": str(price_update)[1:-1]
+                    }
+            requests.post(twitter_applet, json=data)
+            requests.post(telegram_applet, json=data)
+        price_update = "{}: ${}".format(price_update[0], price_update[1])
+        arr.append(price_update)
+        # send latest five prices in time intervals
+        if len(arr) == 5:
+            arr = "<br>".join(arr)
+            data = {"value1": "Random Update ",
+                    "value2": arr
+                    }
+            requests.post(twitter_applet, json=data)
+            requests.post(telegram_applet, json=data)
+            arr = []
+            return None
+        time.sleep(sleepTime)
+
+
+# this is is command line utility function,
+# that takes the argument, parse it ans then call the run function
+def main():
+    parser = argparse.ArgumentParser(description="Bitcoin Notifier")
+    # command line variable for time interval between prices
+    parser.add_argument("-t", "--interval", type=int, nargs=1,
+                        metavar="Interval", default=[2],
+                        help="Time interval in seconds")
+    # command line variable for Lower Limit In USD
+    parser.add_argument("-l", "--lowerLimit", type=int, nargs=1,
+                        metavar="Lower Limit", default=[9600],
+                        help="Lower Limit In USD")
+    # command line variable for Upper Limit In USD
+    parser.add_argument("-u", "--upperLimit", type=int, nargs=1,
+                        metavar="Upper Limit", default=[9800],
+                        help="Upper Limit In USD")
+    new_args = parser.parse_args()
+    print('Running A Program with Interval of =',
+          new_args.interval[0], "Seconds.",
+          'Upper Limit =', new_args.upperLimit[0],
+          'Lower Limit =', new_args.lowerLimit[0])
+    # calls the run function
+    send_data(new_args.interval[0],
+              new_args.lowerLimit[0], new_args.upperLimit[0])
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,65 @@
+# Bitcoin Price Notification Project
+
+## Project Overview
+
+1. This Project will send notification of bitcoin latest price for every one hour.
+2. The notifications will be sent to telegram channel of link https://t.me/bitcoin_notification
+3. The notifications will be sent to twitter also on account of https://twitter.com/amol17398
+4. Used IFTTT for sending notification
+5. Used Flake 8 For linting the program file
+
+## Download
+
+```
+Download the file from the following url :-
+https://github.com/amol-vaditake-au6/Bitcoin-Project.git
+
+(click on the "clone or download" button on your left --> download ZIP)
+```
+
+## Help
+
+```
+optional arguments:
+  -h, --help         show this help message and exit
+  -t Interval,       --interval Interval
+                        Time interval in seconds
+  -u Lower Limit,    --lowerLimit Lower Limit
+                        Lower Limit In USD
+  -l Upper Limit,    --upperLimit Upper Limit
+                        Upper Limit In USD
+```
+
+## How to Run
+
+```
+Go to the directory/root folder where you have downloaded the file.(Preferable using command line)
+
+Now type the following command:-
+
+python project.py -t 60 -l 9500 -u 10000
+
+where "-i" is the command line argument for time interval and "-l" for lower Limit and "-u" for upper limit.
+Feel free to use argument of your choice.
+```
+
+## Notification
+
+```
+There are two ifttt applets used to get notification
+1. Telegram
+     https://t.me/bitcoin_notification
+     (Now Join this channel to get the notification on Telegram)
+2. Twitter
+     https://twitter.com/amol17398
+     (Follow this account to get the notification on Twitter)
+```
+
+## Video Guide
+
+```
+Watch this video for better understanding:
+
+Link--->
+
+```
